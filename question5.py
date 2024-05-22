555aa5d91c53b71a4bf6588220d18882debb25a9
Shree Ram Sundar <153998177+shreeramsundart@users.noreply.github.com>
2024-05-22 11:01:15 +0530

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,1 +1,13 @@
 #write a program to find if the given number is prime no or not
+
+
+num = int(input("Enter a number: "))
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, "is a composite number.")
+            break
+    else:
+        print(num, "is a prime number.")
+else:
+    print(num, "is not a prime number.")
